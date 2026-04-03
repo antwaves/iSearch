9d6943a57aa9098cadd58da834fbca1080e370b9
Annie <184776428+antwaves@users.noreply.github.com>
2026-04-02 21:27:02 -0400

    Improve the query engine a little

--- src/query_engine.py
+++ src/query_engine.py
@@ -17,20 +17,17 @@ def to_terms(doc, translator, term_finder):
 
 def get_vector_tf(query_terms, doc_terms):
     length = len(doc_terms)
-    return {term : doc_terms.count(term) / length for term in query_terms}
+    count = Counter(doc_terms)
+    return {term : count[term] / length for term in query_terms}
 
 
-async def get_vector_idf(session_maker, terms, total_pages):
+async def get_vector_idf(terms, term_total_pages, total_pages):
     idf_vector = {}
-
-    async with session_maker() as session:
-        term_pages = await get_total_pages_for_terms(session, terms)
-
-    term_pages = dict(term_pages)
+    term_pages = dict(term_total_pages)
     for term in terms:
         pages = term_pages.get(term, 0)
         if pages:
-            idf_vector[term] = 1 + log10(total_pages + 1 / term_pages[term])
+            idf_vector[term] = 1 + log10(total_pages / term_pages[term])
         else:
             idf_vector[term] = 1
     
@@ -52,46 +49,69 @@ def dict_dot_prod(dict_one, dict_two):
 
 def get_cosine_simmilarity(vec_one, vec_two):
     dot_prod = dict_dot_prod(vec_one, vec_two)
-    vec_one_len = np.sqrt(dict_dot_prod(vec_one, vec_one))
-    vec_two_len =  np.sqrt(dict_dot_prod(vec_two, vec_two))
+    vec_one_len = np.sqrt(sum(item * item for item in vec_one.values()))
+    vec_two_len =  np.sqrt(sum(item * item for item in vec_two.values()))
+
+    if vec_one_len == 0 or vec_two_len == 0:
+        return 0
     return dot_prod / (vec_one_len * vec_two_len)
 
 
+def link_rank(url, query_terms):
+    score = 0
+    for term in query_terms:
+        if term in url.lower():
+            score += 0.15
+    return score
+
+
 async def main():
-    punctuation_replacements = {}
-    for char in punctuation_replacements:
-        punctuation_replacements[char] = " "
-    translator = str.maketrans(punctuation_replacements)
+    punctuation = {'.': ' ', '?': ' ', '!': ' ', ',': ' ', 
+                    ':': ' ', ';': ' ', '—': ' ', '(': ' ', ')': ' ', 
+                    '[': ' ', ']': ' ', '{': ' ', '}': ' ', '\\': ' ', "'": ' ',
+                    '"': ' ', '/': ' ', '*': ' ', '&': ' ', '~': ' ', '+': ' '}
+    translator = str.maketrans(punctuation)
     term_finder = re.compile(r"[A-Za-z0-9_\-#@]+")
     
     session_maker = await connect_to_db(15)
 
     async with session_maker() as session:
         total_pages = float(await count_pages(session))
+    
+    query = None
+    while query != "(quit)":
+        query = input("New input!: ")
 
-    t = time.time()
-    
-    query = "american war crime in iran"
-    q_terms = to_terms(query, translator, term_finder)
-    q_idf = await get_vector_idf(session_maker, q_terms, total_pages)
-    query_tf_idf = get_tf_idf(q_terms, q_terms, q_idf)
+        q_terms = to_terms(query, translator, term_finder)
 
-    all_pages = []
-    async with session_maker() as session:
-        for term in q_terms:
-            all_pages.extend(await retrieve_term_pages(session, term))
-    
-    all_pages = set(all_pages)
-    scores = []
-    for item in all_pages:
-        page_url, page_content = item
-        terms = to_terms(page_content, translator, term_finder)
-        tf_idf = get_tf_idf(q_terms, terms, q_idf)
-        scores.append([page_url, get_cosine_simmilarity(query_tf_idf, tf_idf)])
-    
-    scores = sorted(scores, key=lambda x: x[1], reverse=True)
-    for score in scores[:20]:
-        print(score[0], score[1])
+        async with session_maker() as session:
+            term_total_pages = await get_total_pages_for_terms(session, q_terms)
+
+        q_idf = await get_vector_idf(q_terms, term_total_pages, total_pages)
+        query_tf_idf = get_tf_idf(q_terms, q_terms, q_idf)
+
+        all_pages = []
+        async with session_maker() as session:
+            for term in q_terms:
+                all_pages.extend(await retrieve_term_pages(session, term))
+       
+        all_pages = set(all_pages)
+        scores = []
+        for item in all_pages:
+            page_url, page_content = item
+            terms = to_terms(page_content, translator, term_finder)
+
+            tf_idf = get_tf_idf(q_terms, terms, q_idf)
+            simmilarity = get_cosine_simmilarity(query_tf_idf, tf_idf)
+            l_rank = 1 + link_rank(page_url, q_terms)
+
+            score = (simmilarity ** 1.2) * (l_rank)
+
+            scores.append([page_url, score, [simmilarity, l_rank]])
+
+        scores = sorted(scores, key=lambda x: x[1], reverse=True)
+        for score in scores[:20]:
+            print(score[0], score[1], score[2][1])
 
 
 if __name__ == "__main__":
